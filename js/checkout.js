e53632005e5751a87bd6f12922eb4e8683c28be2
ZanderBez <221205@virtualwindow.co.za>
2023-09-02 23:05:54 +0200

    Updated cards and model sheet

--- js/checkout.js
+++ js/checkout.js
@@ -1,14 +1,12 @@
-// Retrieve the selected cruises from local storage
 const selectedCruises = JSON.parse(localStorage.getItem('selectedCruises'));
 
-// Check if there are selected cruises to display
+
 if (selectedCruises && selectedCruises.length > 0) {
-    // Loop through the selected cruises and display their details
+    
     selectedCruises.forEach(function (selectedCruise, index) {
-        // Create a new row for each cruise
+       
         const newRow = document.createElement('tr');
 
-        // Create table data (cell) for cruise name, total days, and price
         const cruiseNameCell = document.createElement('td');
         cruiseNameCell.textContent = selectedCruise.name;
 
@@ -18,33 +16,47 @@ if (selectedCruises && selectedCruises.length > 0) {
         const priceCell = document.createElement('td');
         priceCell.textContent = `$${selectedCruise.price}`;
 
-        // Create a "Remove" button
         const removeButtonCell = document.createElement('td');
         const removeButton = document.createElement('button');
         removeButton.textContent = 'Remove';
-        removeButton.className = 'btn btn-danger';
+        removeButton.className = 'btn btn-secondary';
         removeButton.addEventListener('click', function () {
-            // Remove the selected cruise from the table and local storage
             selectedCruises.splice(index, 1);
             localStorage.setItem('selectedCruises', JSON.stringify(selectedCruises));
-            newRow.remove(); // Remove the row from the table
+            newRow.remove();
         });
 
-        // Append cells and button to the new row
         newRow.appendChild(cruiseNameCell);
         newRow.appendChild(totalDaysCell);
         newRow.appendChild(priceCell);
         newRow.appendChild(removeButtonCell);
         removeButtonCell.appendChild(removeButton);
 
-        // Append the new row to the table body
         const cruiseDetailsBody = document.getElementById('cruise-details-body');
         cruiseDetailsBody.appendChild(newRow);
-    });
-} else {
-    // Display a message if no cruises are selected
-    console.log("No cruises selected.");
+        });
+
+        document.getElementById('remove-all-button').addEventListener('click', function () {
+        removeAllCruises();
+        });
+
+    function removeAllCruises() {
+        selectedCruises.length = 0;
+        localStorage.removeItem('selectedCruises');
+        const cruiseDetailsBody = document.getElementById('cruise-details-body');
+        cruiseDetailsBody.innerHTML = '';
+    }
 }
+
+let popup = document.getElementById("popup");
+
+function openPopup(){
+    popup.classList.add("open-popup");
+}
+function closePopup(){
+    popup.classList.remove("open-popup");
+}
+
 
 
 
